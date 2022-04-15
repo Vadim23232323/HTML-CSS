3431804a19e538cd5c997d094a48615baa9b3aa3
Vadim <32672528+Vadim23232323@users.noreply.github.com>
2022-04-15 09:37:37 +0300

    Finish web site

--- Udemy_HTML+CSS/WEB_SITE/js/main.js
+++ Udemy_HTML+CSS/WEB_SITE/js/main.js
@@ -1,5 +1,17 @@
 window.addEventListener("DOMContentLoaded", () => {
-    const tabs = document.querySelectorAll(".tabs_item");
+    const tabs = document.querySelectorAll(".tabs_item"),
+    playBtn=document.querySelector("#playVideo"),
+    video=document.querySelector("#video");
+    playBtn.addEventListener('click', function() {
+    video.play();
+    video.setAttribute('controls','controls');
+    playBtn.style.display="none";
+    });
+    video.addEventListener('ended', function() {
+        this.src= this.src;
+        playBtn.style.display = "block";
+        video.removeAttribute('controls');
+    })
     tabs.forEach(elem => {
         elem.addEventListener("click", () => {
             tabs.forEach(otherItem => {
@@ -12,8 +24,26 @@ window.addEventListener("DOMContentLoaded", () => {
     });
     $('.carousel').slick({
         autoplay: true,
-        autoplaySpeed: 2000,
+        autoplaySpeed: 5000,
         prevArrow: '<div class="carousel_arrow carousel_prev"><i class="fa-solid fa-angle-left"></i></div>',
         nextArrow: '<div class="carousel_arrow carousel_next"><i class="fa-solid fa-angle-right"></i></div>'
     });
+    $('#buyNowBtn').on('click', popupOpen);
+    $('#TryForFree').on('click', popupOpen);
+    $('.popup_close').on('click', () => {
+        $('.popup').removeClass('popup_active');
+        $('body').css('overflow', 'visible')
+    });
+    function popupOpen() {
+        $('.popup').addClass('popup_active');
+        $('body').css('overflow', 'hidden')
+    }
+    $("a[href*='#']").on("click", function(e){
+        var anchor = $(this);
+        $('html, body').stop().animate({
+          scrollTop: $(anchor.attr('href')).offset().top
+        }, 777);
+        e.preventDefault();
+        return false;
+      });
 });
